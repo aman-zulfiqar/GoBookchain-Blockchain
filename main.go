a60592366e84a002511389486159aa404b8508d4
aman-zulfiqar <amanzulfiqar75@gmail.com>
2025-05-20 19:50:40 +0500

    Updated

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"crypto/md5"
+	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 
@@ -35,6 +39,55 @@ type BlockChain struct {
 }
 
 var Blockchain *BlockChain
+
+func writeblock(w *http.ResponseWriter, r *http.Request){
+	var checkoutitem BookCheckout
+
+	if err := json.NewDecoder(r.Body).Decode(&BookCheckout); err != nil{
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Printf("Could not Write the book checkout:%v", err)
+		w.Write([]byte("could not Write the book"))
+		return
+	}
+
+	BlockChain.AddBlock(checkoutitem)
+
+	resp, err := json.MarshalIndent(checkoutitem, "", " ")
+	if err != nil{
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Printf("Could not marshal payload:%v", err)
+		w.Write([]byte("could not save the checkoutdata"))
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(resp)
+}
+
+func newBook(w *http.ResponseWriter, r *http.Request) {
+	var book Book
+
+	if err := json.NewDecoder(r.Body).Decode(&book); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Printf("Could not create new book:%v", err)
+		w.Write([]byte("could not create the new book"))
+		return
+	}
+
+	h := md5.New()
+	io.WriteString(h, book.ISBN + book.PublishDate)
+	book.ID = fmt.Sprintf("%x", h.Sum(nil))
+
+	resp, err := json.MarshalIndent(book, "", " ")
+	if err != nil{
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Printf("could not marshal the oayload:%v", err)
+		w.Write([]byte("could not save the book data"))
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(resp)
+}
 
 func main() {
 	r := mux.NewRouter()
